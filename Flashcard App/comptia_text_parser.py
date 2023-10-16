c88cab780ee839ba28790290b6d0ba1e76ea4817
CLN <cnuenthel@gmail.com>
2023-10-16 07:28:34 -0500

    updated to flashcard db

---
+++ Flashcard App/comptia_text_parser.py
@@ -0,0 +1,14 @@
+import json
+
+def parse_text(file_name, group_name):
+    with open(file_name, "r") as f:
+        data = f.read()
+    newlist = [item.strip() for item in data.split("â€¢") if item != "â€¢"]
+    newdict = {item: "" for item in newlist[1:]}
+    data = {group_name: newdict}
+
+    with open("flashcards.json", "w") as f:
+        json.dump(data, f, indent=2)
+
+    print("Data Parsed!")
+
